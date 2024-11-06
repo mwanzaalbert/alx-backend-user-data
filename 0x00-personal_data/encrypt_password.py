02c420abff89069df57f7d3c7cc70ba444a854aa
mwanzaalbert <user.email>
2024-11-06 12:43:44 +0300

    pycodestyle formatting

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+""" password encryption."""
 import bcrypt
+
 
 def hash_password(password: str) -> bytes:
     """
     Hashes a password with a randomly generated salt using bcrypt.
-    
+
     Args:
         password (str): The password to hash.
-        
+
     Returns:
         bytes: The salted, hashed password as a byte string.
     """
@@ -16,14 +19,15 @@ def hash_password(password: str) -> bytes:
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """
     Validates that the provided password matches the hashed password.
-    
+
     Args:
         hashed_password (bytes): The previously hashed password.
         password (str): The plain-text password to verify.
-        
+
     Returns:
         bool: True if the password is valid, False otherwise.
     """
@@ -35,7 +39,7 @@ if __name__ == "__main__":
     password = "MyAmazingPassw0rd"
     print(hash_password(password))
     print(hash_password(password))
-    
+
     encrypted_password = hash_password(password)
     print(encrypted_password)
     print(is_valid(encrypted_password, password))
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 """
 Filtered logger module
@@ -16,9 +17,14 @@ from mysql.connector.connection import MySQLConnection
 # Define the fields that are considered PII
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Filter data."""
+
     pattern = f"({'|'.join(fields)})=([^;]+)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class """
@@ -32,20 +38,22 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
+
 
 def get_logger() -> logging.Logger:
     """Creates and returns a logger with specified settings."""
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
-    
+
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
-    
+
     logger.addHandler(stream_handler)
-    
+
     return logger
 
 
@@ -66,16 +74,18 @@ def get_db() -> MySQLConnection:
         database=database
     )
 
+
 def format_row(row: Tuple[str], headers: List[str]) -> str:
     """
     Formats a database row into a log message string.
     """
-    return "; ".join(f"{header}={value}" for header, value in zip(headers, row))
+    return "; ".join(f"{header}={value}" for header, value in zip(headers,
+                                                                  row))
 
 
 def main():
     """
-    Main function to retrieve all rows from the users table and log each row with
+    Retrieve all rows from the users table and log each row with
     sensitive data filtered.
     """
     # Configure logging with RedactingFormatter
@@ -96,31 +106,6 @@ def main():
     cursor.close()
     db.close()
 
+
 if __name__ == "__main__":
     main()
-#     fields = ["password", "date_of_birth"]
-#     messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-#                 "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
-# 
-#     for message in messages:
-#         print(filter_datum(fields, 'xxx', message, ';'))
-#     print()
-#     
-#     message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
-#     log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
-#     formatter = RedactingFormatter(fields=("email", "ssn", "password"))
-#     print(formatter.format(log_record))
-#     
-#     print()
-#     
-#     print(get_logger.__annotations__.get('return'))
-#     print("PII_FIELDS: {}".format(len(PII_FIELDS)))
-#     
-#     print()
-#     db = get_db()
-#     cursor = db.cursor()
-#     cursor.execute("SELECT COUNT(*) FROM users;")
-#     for row in cursor:
-#         print(row[0])
-#     cursor.close()
-#     db.close()
